b9d4e5b1bf5d14b89d5179cdba818a59988a9d02
Vsevolod Iursha <vsevolod.yursha@yandex.ru>
2017-06-13 14:00:17 +0300

    Created
    Выполнено задание 6

---
+++ Pr2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Выводит на экран кол-во аргументов командной строки
+            System.Console.WriteLine(args.Length);
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
